4d8a44db4686928bbfa41697a7e80a82f724b430
viCppDev <vjmoralesmora@gmail.com>
2022-08-04 21:48:54 +0200

    Added Accumulator, Program Counter and Input Latch Data registers

---
+++ core/registers/Accumulator.vhd
@@ -0,0 +1,37 @@
+LIBRARY ieee ;
+USE ieee.std_logic_1164.all ;
+
+ENTITY Accumulator  IS
+    PORT ( 
+        ctrl : IN STD_LOGIC;
+        dALU : INOUT STD_LOGIC_VECTOR(7 downto 0);
+        data : INOUT STD_LOGIC_VECTOR(7 downto 0)) ;
+END Accumulator ;
+
+ARCHITECTURE LogicFunction OF Accumulator IS
+    -- Internal register
+    signal intData : STD_LOGIC_VECTOR(7 downto 0);
+BEGIN
+    
+    statesReg : PROCESS (ctrl, dALU, data)  
+    BEGIN
+        IF(ctrl='Z') THEN
+            data <= (others=>'Z');
+            dALU <= (others=>'Z');
+        ELSIF (ctrl='0') THEN
+                dALU <= intData;
+                data <= intData;
+        ELSIF (ctrl='1') THEN 
+            -- SET FROM TWO SOURCES IN CASE OF CONFLICT **DONT WRITE**
+            IF ((data = "ZZZZZZZZ") and  (dALU /= "ZZZZZZZZ")) THEN
+                intData <= dALU;
+            ELSIF ((data /= "ZZZZZZZZ") and  (dALU = "ZZZZZZZZ")) THEN
+                intData <= data;
+            ELSIF ((data /= "ZZZZZZZZ") and  (dALU /= "ZZZZZZZZ")) THEN
+                --NOTHING
+            END IF;
+        END IF ;
+    END PROCESS statesReg;
+
+
+END LogicFunction ;
---
+++ core/registers/Input_Latch_Data.vhd
@@ -0,0 +1,31 @@
+LIBRARY ieee ;
+USE ieee.std_logic_1164.all ;
+
+ENTITY Input_Latch_Data IS
+    PORT ( 
+        ctrl : IN STD_LOGIC;
+        data : IN STD_LOGIC_VECTOR(7 downto 0);
+        addr : INOUT STD_LOGIC_VECTOR(15 downto 0)) ;
+END Input_Latch_Data ;
+
+ARCHITECTURE LogicFunction OF Input_Latch_Data IS
+    -- Internal register
+    alias addrH is addr(15 downto 8);
+    alias addrL is addr(7 downto 0);
+    signal intData : STD_LOGIC_VECTOR(7 downto 0);
+BEGIN
+    
+statesReg : PROCESS (ctrl, addr, addrH, addrL, data)  
+BEGIN
+    IF(ctrl='Z') THEN
+        addr <= "ZZZZZZZZZZZZZZZZ";
+    ELSIF (ctrl='0') THEN
+        addrH <= data;
+        addrL <= intData;
+    ELSIF (ctrl='1') THEN 
+        intData <= data;
+    END IF ;
+END PROCESS statesReg;
+
+
+END LogicFunction ;
